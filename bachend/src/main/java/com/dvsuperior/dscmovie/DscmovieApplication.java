726d602ce173b6ee194398a07b7d1b1b1466f4a6
Fernando Leite <fbrasilienseleite@gmail.com>
2022-03-28 20:28:27 -0300

    Project created

---
+++ bachend/src/main/java/com/dvsuperior/dscmovie/DscmovieApplication.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dscmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscmovieApplication.class, args);
+	}
+
+}
